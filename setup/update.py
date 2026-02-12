74bc074afa59432d9ca8d23fc9ea0dd36c88e2af
rienovie <fisher.vince@gmail.com>
2026-02-12 06:43:29 -0500

    Update script - version 0.0.2

--- setup/update.py
+++ setup/update.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
 
 import os
-import sys
 import xml.etree.ElementTree as ET
-# import urllib.request
+import urllib.request
 
 
 if not os.path.exists("resources"):
+    print("Resources directory not found")
     print("Script must be run from homePotato root directory")
-    sys.exit(1)
+    if input("If you wish to run the script anyway, please type 'yes' and hit enter: ") != "yes":
+        exit(0)
 
-# if not os.path.exists("tmp"):
-#     os.mkdir("tmp")
-# if os.path.exists("tmp/atom.xml"):
-#     os.remove("tmp/atom.xml")
+if not os.path.exists("tmp"):
+    os.mkdir("tmp")
+else:
+    os.system("rm -rf tmp")
+    os.mkdir("tmp")
+
+if not os.path.exists("resources/local/version.txt"):
+    with open("resources/local/version.txt", "w") as f:
+        f.write("0.0.0")
 
 with open("resources/local/version.txt", "r") as f:
     curVersion = f.read().strip()
@@ -22,14 +28,40 @@ print("Current Version: " + curVersion)
 
 xml_url = "https://github.com/rienovie/homePotato/releases.atom"
 
-# urllib.request.urlretrieve(xml_url, "tmp/atom.xml")
+urllib.request.urlretrieve(xml_url, "tmp/atom.xml")
 tree = ET.parse("tmp/atom.xml")
 root = tree.getroot()
 
+prefix = "{http://www.w3.org/2005/Atom}"
+versions = []
 # comes with prefix
 for item in root:
-    item.tag = item.tag.removeprefix("{http://www.w3.org/2005/Atom}")
-    print(item.tag)
+    if item.tag == f"{prefix}entry":
+        for child in item:
+            if child.tag == f"{prefix}title":
+                if child.text is not None and child.text.startswith("Release_"):
+                    versions.append(child.text.split("_")[-1])
 
+versions.sort(reverse=True)
+
+if versions[0] <= curVersion:
+    print("System is up to date")
+    exit(0)
+
+print("Update available!")
+print("Current Version: " + curVersion + " -> New Version: " + versions[0])
+
+download_url = "https://github.com/rienovie/homePotato/archive/refs/tags/v" + versions[0] + ".zip"
+
+urllib.request.urlretrieve(download_url, "tmp/update.zip")
+os.system("unzip tmp/update.zip -d tmp")
+os.remove("tmp/update.zip")
+os.system(f"cp -rf tmp/homePotato-{versions[0]}/* .")
+os.system("rm -rf tmp")
+
+with open("resources/local/version.txt", "w") as f:
+    f.write(versions[0])
+
+print("System update complete")
 
 print("Script complete")
